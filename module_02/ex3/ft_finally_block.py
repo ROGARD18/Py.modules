521a1e1402a1e18ff89d654e73c3f60b6cdbb19d
Antoine Rogard <a18ntoinecbr@icloud.com>
2026-01-16 12:04:34 +0100

    all

--- module_02/ex3/ft_finally_block.py
+++ module_02/ex3/ft_finally_block.py
@@ -1,22 +1,20 @@
-def water_plants(plant_list):
+def water_plants(plant_list) -> None:
     try:
         for plant in plant_list:
-            try:
-                plant is not None
-                print(f"Watering {plant}")
-            except ValueError:
-                print(f"Error: Cannot water {plant} - invalid plant!")
+            print("Watering" + plant)
+    except Exception as e:
+        print(f"Error: Cannot water {plant} - invalid plant!")
     finally:
         print("Closing watering system (cleanup)")
 
-def test_watering_system():
+def test_watering_system() -> None:
     print("Testing normal watering...")
     water_plants(["tomato", "lettuce"])
     print("\nTesting with error...")
     water_plants(["tomato", None, "carrots"])
 
 
-def main():
+def main() -> None:
     test_watering_system()
 
 
--- module_02/ex4/ft_raise_errors.py
+++ module_02/ex4/ft_raise_errors.py
@@ -1,2 +1,25 @@
+def check_plant_health(plant_name, water_level, sunlight_hours) -> None:
+    try:
+        if plant_name == "":
+            raise ValueError("Error: Plant name cannot be empty!")
+        if water_level > 10 or water_level < 1:
+            raise ValueError("Error : Water level is not valid")
+        if sunlight_hours > 12 or sunlight_hours < 2:
+            raise ValueError("Error : Sunlight_hours is not valid")
+        print("SUCCESS : everything ok")
+    except Exception as e:
+        print(f"{e}")
+
+def test_plant_checks() -> None:
+    check_plant_health("rose", 5, 5)
+    check_plant_health("", -1, -1)
+    check_plant_health("rose", -1, 3)
+    check_plant_health("rose", 1, -1)
 
 
+def main() -> None:
+    test_plant_checks()
+
+
+if __name__=="__main__":
+    main()
