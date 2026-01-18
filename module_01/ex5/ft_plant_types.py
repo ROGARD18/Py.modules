6fefe406a103ad3f6b5e722ecdb71439fc23f4a3
Antoine Rogard <anrogard@z3r9p6.42lyon.fr>
2026-01-18 21:43:37 +0100

    almost finish last exercice of module 01

--- module_01/ex5/ft_plant_types.py
+++ module_01/ex5/ft_plant_types.py
@@ -10,6 +10,7 @@ class Plant:
 
 class Flower(Plant):
     """Specialized plant that can bloom"""
+
     def __init__(self, name: str, height: int, age: int, color: str) -> None:
         """Initialize flower with color."""
         super().__init__(name=name, height=height, age=age)
@@ -25,8 +26,12 @@ class Flower(Plant):
 
 class Tree(Plant):
     """Specialized plant that provides shade"""
-    def __init__(self, name: str, height: int, age: int,
-                 trunk_diameter: int) -> None:
+
+    def __init__(
+            self,
+            name: str,
+            height: int,
+            age: int, trunk_diameter: int) -> None:
         """Initialize tree with trunk diameter."""
         super().__init__(name=name, height=height, age=age)
         self.trunk_diameter: int = trunk_diameter
@@ -39,8 +44,14 @@ class Tree(Plant):
 
 class Vegetables(Plant):
     """Specialized plant for food"""
-    def __init__(self, name: str, height: int, age: int, harvest_season: int,
-                 nutritional_value: str) -> None:
+
+    def __init__(
+            self,
+            name: str,
+            height: int,
+            age: int,
+            harvest_season: int,
+            nutritional_value: str, ) -> None:
         """Initialise Vegetables with nutritional_value"""
         super().__init__(name=name, height=height, age=age)
         self.harvest_season: str = harvest_season
@@ -54,13 +65,16 @@ def main() -> None:
     tomato = Vegetables("Tomato", 80, 90, "summer", "vitamin C")
 
     print("Garden Plant Types")
-    print(f"{rose.name} (Flower): {rose.height}cm, {rose.age} days, {rose.color} color")
+    print(f"{rose.name} (Flower): {rose.height}cm, {rose.age}", end="")
+    print(f"days, {rose.color} color")
     rose.bloom()
-    
-    print(f"{oak.name} (Tree): {oak.height}cm, {oak.age} days, {oak.trunk_diameter}cm diameter")
+
+    print(f"{oak.name} (Tree): {oak.height}cm, {oak.age}", end="")
+    print(f"days, {oak.trunk_diameter}cm diameter")
     oak.produce_shade()
-    
-    print(f"{tomato.name} (Vegetable): {tomato.height}cm, {tomato.age} days, {tomato.harvest_season} harvest")
+
+    print(f"{tomato.name} (Veget): {tomato.height}cm, {tomato.age}", end="")
+    print("days, {tomato.harvest_season} harvest")
     print(f"{tomato.name} is rich in {tomato.nutritional_value}")
 
 
--- module_01/ex6/ft_garden_analytics.py
+++ module_01/ex6/ft_garden_analytics.py
@@ -1,24 +1,102 @@
 class Plant:
     def __init__(self, name: str, height: int):
+        self.name: str = name
+        self.height_start: int = height
+        self.new_height: int = height
+
+
+class FloweringPlant(Plant):
+    def __init__(self, name: str, height: int, color: str) -> None:
+        super().__init__(name=name, height=height)
+        self.color: str = color
+
+
+class PrizeFlower(FloweringPlant):
+    def __init__(self, name: int, height: int, color: str, prize: int) -> None:
+        super().__init__(name, height, color)
+        self.prize: int = prize
 
 
 class GardenManager:
+    garden_numbers: int = 0
 
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name: str = name
         self.plants_list: list = []
+        self.garden_stats = self.GardenStats()
 
     class GardenStats:
 
-        def get_plants_number(self) -> int:
-            plants_number = 0
-            for plant in self.plants_list:
+        @staticmethod
+        def get_plants_in_garden(plants_list: list[Plant]) -> None:
+            print("Plants in garden :")
+            for plant in plants_list:
                 if isinstance(plant, PrizeFlower):
-                    prize_count += 1
+                    print(f"- {plant.name} : {plant.new_height}cm, ", end="")
+                    print(f"{plant.color}, Prize points: {plant.prize}")
                 elif isinstance(plant, FloweringPlant):
-                    flowering_count += 1
+                    print(f"- {plant.name}: {plant.new_height}cm", end="")
+                    print(f", {plant.color} flowers (bloomig)")
                 else:
-                    regular_count += 1
-                plants_number += 1
-            return (plants_number)
-        
+                    print(f"- {plant.name} : {plant.new_height}cm")
+
+        @staticmethod
+        def print_numbers_plant(plants_list: list) -> None:
+            count_regular: int = 0
+            count_flower: int = 0
+            count_prize: int = 0
+            count_tot: int = 0
+            diff_height: int = 0
+            for plant in plants_list:
+                diff_height += plant.new_height - plant.height_start
+                if isinstance(plant, PrizeFlower):
+                    count_prize += 1
+                elif isinstance(plant, FloweringPlant):
+                    count_flower += 1
+                else:
+                    count_regular += 1
+            count_tot = count_prize + count_flower + count_regular
+            print(f"Plants added: {count_tot}, Total growth: {diff_height}cm")
+            print(f"Plant types: {count_regular} regular", end="")
+            print(f" {count_flower} flower, {count_prize} prize", end="")
+
+        @staticmethod
+        def all_stats(plants_list: list) -> None:
+            GardenManager.GardenStats.get_plants_in_garden(plants_list)
+            GardenManager.GardenStats.print_numbers_plant(plants_list)
+
+    @staticmethod
+    def check_height(height: int) -> bool:
+        return height >= 0
+
+    def add_plant(self, plant: Plant, plants_list: list) -> None:
+        if GardenManager.check_height(plant.new_height):
+            plants_list.append(plant)
+            print(f"Added {plant.name} to {self.name}'s garden")
+        else:
+            print(f"\nCannot add {plant.name}: {plant.new_height} invalid\n")
+
+    @classmethod
+    def grow_all_plants(self, plants_list: list) -> None:
+        for plant in plants_list:
+            plant.new_height += 1
+
+
+def main() -> None:
+    # create plants
+    print("=== Garden Management System Demo ===\n")
+    Oak = Plant("Oak", 101)
+    Rose = FloweringPlant("Rose", 26, "red")
+    Sunflower = PrizeFlower("Sunflower", 51, "yellow", 10)
+    Alice = GardenManager("Alice")
+    Alice.add_plant(Oak, Alice.plants_list)
+    Alice.add_plant(Rose, Alice.plants_list)
+    Alice.add_plant(Sunflower, Alice.plants_list)
+    print("")
+    Alice.grow_all_plants(Alice.plants_list)
+    print("=== Alice's garden Report ===")
+    Alice.GardenStats.all_stats(Alice.plants_list)
+
+
+if __name__ == "__main__":
+    main()
