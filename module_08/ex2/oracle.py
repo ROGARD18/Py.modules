c3424be80f603c9f31f4f7001b405978717fab97
Antoine Rogard <anrogard@z4r12p6.42lyon.fr>
2026-02-17 17:20:47 +0100

    finish ex2 module08 so finish module

---
+++ module_08/ex2/oracle.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+
+
+def error_handling(data: str) -> None:
+    print(f"Error: No {data} found")
+
+
+def oracle() -> None:
+    load_dotenv()
+    MATRIX_MODE = os.getenv('MATRIX_MODE')
+    if MATRIX_MODE is None:
+        return error_handling("MATRIX_MODE")
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    if DATABASE_URL is None:
+        return error_handling("DATABASE_URL")
+    API_KEY = os.getenv('API_KEY')
+    if API_KEY is None:
+        return error_handling("API_KEY")
+    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    if LOG_LEVEL is None:
+        return error_handling("LOG_LEVEL")
+    ZION_ENDPOINT = os.getenv('ZION_ENDPOINT')
+    if ZION_ENDPOINT is None:
+        return error_handling("ZION_ENDPOINT")
+    print("\nORACLE STATUS: Reading the Matrix...\n")
+
+    print("Configuration loaded:")
+    print("Mode:", MATRIX_MODE)
+    print("Database: Connected to local instance")
+    print("API Access: Authenticated")
+    print("Log Level:", LOG_LEVEL)
+    print("Zion Network:", ZION_ENDPOINT)
+
+    print("\n")
+    print("Environment security check:")
+    print("[OK] No hardcoded secrets detected")
+    print("[OK] .env file properly conigured")
+    print("[OK] Production overrides available")
+
+    print("\nThe Oracle sees all configurations.")
+
+
+if __name__ == "__main__":
+    oracle()
