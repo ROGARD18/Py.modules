3c16d4e8f2ef10101d087e997a8c23f71d303350
Antoine Rogard <anrogard@z4r6p4.42lyon.fr>
2026-02-24 19:55:25 +0100

    add spell_combiner to ex1/module10

--- module_10/ex1/higher_magic.py
+++ module_10/ex1/higher_magic.py
@@ -7,7 +7,7 @@ def spell_heal(healed: str) -> str:
 
 
 def spell_combiner(spell1: callable, spell2: callable) -> callable:
-    return
+    return (spell1 + ", " + spell2)
 
 
 def power_amplifier(base_spell: callable, multiplayer: int) -> callable:
@@ -23,11 +23,17 @@ def spell_sequence(spells: list[callable]) -> callable:
 
 
 def main() -> None:
-    pass
+    # test_values = [12, 9, 7]
+    test_targets = ['Dragon', 'Fireball', 'Goblin', 'Wizard', 'Knight']
+    spell1 = spell_hit(test_targets[1], test_targets[0])
+    spell2 = spell_heal(test_targets[0])
+
+    print("\nTesting spell combiner...")
+    print(spell_combiner(spell1, spell2))
+
+    print("\nTesting power amplifier...")
+    print(power_amplifier)
 
 
 if __name__ == "__main__":
-    test_values = [12, 9, 7]
-    test_targets = ['Dragon', 'Goblin', 'Wizard', 'Knight']
     main()
-
