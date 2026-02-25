eddc41170e254b2f583085a51882582e94eb4098
Antoine Rogard <a18ntoinecbr@icloud.com>
2026-02-25 19:13:11 +0100

    finish module10/ex1

--- module_10/ex1/higher_magic.py
+++ module_10/ex1/higher_magic.py
@@ -1,38 +1,59 @@
-def spell_hit(attacker: str, target: str) -> str:
-    return (f'{attacker} hits {target}')
+from typing import Callable
 
 
-def spell_heal(healed: str) -> str:
-    return (f'Heals {healed}')
+def spell_hit(target: str) -> str:
+    return f"hits {target}"
 
 
-def spell_combiner(spell1: callable, spell2: callable) -> callable:
-    return (spell1 + ", " + spell2)
+def spell_heal(target: str) -> str:
+    return f"Heals {target}"
 
 
-def power_amplifier(base_spell: callable, multiplayer: int) -> callable:
-    pass
+def damage_calculator(power: int) -> int:
+    return power
 
 
-def conditional_caster(condition: callable, spell: callable) -> callable:
-    pass
+def is_dragon(target: str) -> bool:
+    return target == "Dragon"
 
 
-def spell_sequence(spells: list[callable]) -> callable:
-    pass
+def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+    return (
+        lambda *args, **kwag: (spell1(*args, **kwag), spell2(*args, **kwag))
+    )
+
+
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+    return lambda *args, **kwargs: base_spell(*args, **kwargs) * multiplier
+
+
+def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        if condition(*args, **kwargs):
+            return spell(*args, **kwargs)
+        return "Spell fizzled"
+    return wrapper
+
+
+def spell_sequence(spells: list[Callable]) -> Callable:
+    return lambda *args, **kwargs: [s(*args, **kwargs) for s in spells]
 
 
 def main() -> None:
-    # test_values = [12, 9, 7]
-    test_targets = ['Dragon', 'Fireball', 'Goblin', 'Wizard', 'Knight']
-    spell1 = spell_hit(test_targets[1], test_targets[0])
-    spell2 = spell_heal(test_targets[0])
+    print("--- Test du Higher Realm ---")
 
-    print("\nTesting spell combiner...")
-    print(spell_combiner(spell1, spell2))
+    combined = spell_combiner(spell_hit, spell_heal)
+    print(f"Combiner (Dragon): {combined('dragon')}")
 
-    print("\nTesting power amplifier...")
-    print(power_amplifier)
+    mega_damage = power_amplifier(damage_calculator, 3)
+    print(f"Amplifier (Base 10, x3): {mega_damage(10)}")
+
+    dragon_only_spell = conditional_caster(is_dragon, spell_hit)
+    print(f"Conditional (Dragon): {dragon_only_spell('Dragon')}")
+    print(f"Conditional (Goblin): {dragon_only_spell('Goblin')}")
+
+    sequence = spell_sequence([spell_hit, spell_heal])
+    print(f"Sequence (Knight): {sequence('Knight')}")
 
 
 if __name__ == "__main__":
